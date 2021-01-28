1ddb01c0ee4f9fd487f9c900962676f9230650b1
Devin Rockwell <devinr@devins-mbp.lan>
2021-01-27 20:08:06 -0500

    basic lexing

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -25,12 +25,25 @@ func NewLexer(source string) Lexer {
 func (lexer *Lexer) advance() rune {
 	lexer.pos++
 	lexer.col++
-	return runeAt(lexer.source, lexer.pos-1)
+	if lexer.pos <= len(lexer.source) {
+		return runeAt(lexer.source, lexer.pos-1)
+	}
+	return rune('\000')
 }
 
 // Lex returns an slice of lines
 func (lexer *Lexer) Lex() ([]token.Line, error) {
-	return []token.Line{}, nil
+	lines := []token.Line{}
+	line := []token.Token{}
+	for c := lexer.advance(); c != '\000'; c = lexer.advance() {
+		switch c {
+		case rune('\n'):
+			lines = append(lines, line)
+			line = []token.Token{}
+		}
+	}
+
+	return lines, nil
 }
 
 func runeAt(str string, idx int) rune {
